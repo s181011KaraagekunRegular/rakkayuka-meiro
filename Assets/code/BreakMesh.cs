33647e539807ea626b05260303feff9ecaae3771
s181011KaraagekunReGular <s181011@kashiwanoha.ed.jp>
2020-11-04 10:14:31 +0900

    2020/11/04とりま

---
+++ Assets/code/BreakMesh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakMesh : MonoBehaviour
+{
+
+    [SerializeField]
+    float breakValue = 0.1f;
+
+    void Start()
+    {
+        List<Vector3> vertextList = new List<Vector3>();
+
+        MeshFilter myMesh = this.gameObject.GetComponent<MeshFilter>();
+
+        Vector3[] vertPos = myMesh.mesh.vertices;
+
+        for (int i = 0; i < vertPos.Length; i++)
+        {
+            vertextList.Add(vertPos[i]);
+            vertextList[i] += new Vector3(Random.Range(-breakValue, breakValue), Random.Range(-breakValue, breakValue), 0);
+        }
+
+        myMesh.mesh.SetVertices(vertextList);
+    }
+}
