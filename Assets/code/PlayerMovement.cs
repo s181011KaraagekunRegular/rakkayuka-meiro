ebd6cc305ffaf1a9f7f7fe7e2d8d5efe8202fd75
s181011KaraagekunReGular <s181011@kashiwanoha.ed.jp>
2020-11-04 10:21:56 +0900

    2020/11/04　ステージ分け
    前：SampleScene
    後：ステージ0
    に変更
    ステージを複数作るためにScenesを新規で作成した。

---
+++ Assets/code/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.2f;
+
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        float moveV = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Translate(moveH, 0.0f, moveV);
+    }
+}
