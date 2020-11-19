403ab767ccb20e350ad43043b66f0325843db826
s181011KaraagekunRegular <s181011@kashiwanoha.ed.jp>
2020-11-19 15:16:38 +0900

    2020/11/19

---
+++ Assets/code/Trans/TransStage0.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TransStage0 : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void OnClick()
+    {
+        // メインシーンへ移動
+        SceneManager.LoadScene("stage0");
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/code/Trans/TransStage1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TransStage1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void OnClick()
+    {
+        // メインシーンへ移動
+        SceneManager.LoadScene("stage1");
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
